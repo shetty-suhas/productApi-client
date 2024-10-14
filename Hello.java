32711bcce9d16026c3b515274fcfd79fa8a9696b
shetty-suhas <shettysuhasc@gmail.com>
2024-10-14 17:38:14 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 import java.util.*; 
 
 public class Hello{ 
-   public static void Main(String[] args){ 
+   public static void main(String[] args){ 
         System.out.println("Hello World"); 
    }
 }
